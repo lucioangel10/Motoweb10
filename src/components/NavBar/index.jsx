87b44ef84722ac10b140a3b7e7313ca8afe2f95c
Lucio Angel <lucio.ivan.angel@gmail.com>
2022-07-05 15:04:38 -0300

    second commit

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './style.css';
+import CartWidget from '../CartWidget';
+function NavBar() {
+    return (
+        <div>
+            
+            <ul className='nav'>
+                <h1 className='nombre'>Motoweb</h1>
+                <a className='aTexto' href='#'>Street</a>
+                <a className='aTexto' href='#'>ON/OFF</a>
+                <a className='aTexto' href='#'>Scooter</a>
+                <CartWidget/>
+            </ul>
+        </div>
+    );
+
+
+
+
+
+    
+}
+
+export default NavBar;
